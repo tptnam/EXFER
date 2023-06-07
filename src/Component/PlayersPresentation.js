67f72055ba29ddc0aeeb6a584329a6bc87cda33e
ThanhNam <trannam.nt865@gmail.com>
2023-06-07 16:32:59 +0700

    Ex10 done

--- src/Component/PlayersPresentation.js
+++ src/Component/PlayersPresentation.js
@@ -1,20 +1,34 @@
 import React from "react";
-export default function PlayersPresentation({ players }) {
-  //console.log(players);
+import { Players } from "../shared/ListOfPlayers";
+import { useState } from "react";
+export default function Player() {
+  const [player, setPlayer] = useState([])
   return (
     <div className="container">
-      {players.map((player) => (
+
+      {Players.map((player) => (
         <div className="column">
           <div className="card">
-            <img src={player.img} alt="Daylaanh" />
+            <img src={player.img} alt="daylahinhanh=))" />
             <h3>{player.name}</h3>
             <p className="title">{player.club}</p>
             <p>
-              <button>Detail</button>
+              <button onClick={() => { setPlayer(player); }}>
+                <a href="#popup1" id="openPopUp"> Detail </a>
+              </button>
             </p>
           </div>
         </div>
       ))}
+
+      <div id="popup1" className="overlay">
+        <div className="popup">
+          <img src={player.img} alt="" />
+          <h2>{player.name}</h2>
+          <a className="close" href="#">&times;</a>
+          <div className="content">{player.info}</div>
+        </div>
+      </div>
     </div>
   );
 }
